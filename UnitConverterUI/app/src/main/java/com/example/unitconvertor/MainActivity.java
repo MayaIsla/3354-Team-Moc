41f48276846a57fc570189d15bf2549502c44399
Alexis Vu <vualexis@outlook.com>
2019-11-24 19:33:10 -0600

    Add constraint layout for converter UI

---
+++ UnitConverterUI/app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.unitconvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+}
