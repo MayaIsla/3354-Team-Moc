715d758a9fe89f1fb4da1bdea1969a2c4279d0db
Alexis Vu <vualexis@outlook.com>
2019-11-24 19:42:59 -0600

    Add main selection spinner - user selects what type of units they want to convert

--- UnitConverterUI/app/src/main/java/com/example/unitconvertor/MainActivity.java
+++ UnitConverterUI/app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -11,5 +11,39 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.unit_options_array,
+                android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(adapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()  {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)  {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) +"", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)  {
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
