4053ac8d039c224f352118df964641e04f2a31b9
Alexis Vu <vualexis@outlook.com>
2019-11-28 03:00:13 -0600

    add getConversions method
    will return ArrayList<String> with all equivalent conversions for easy displaying

--- app/src/main/java/com/moc/sharecalc/ui/unitconverter/UnitConverterFragment.java
+++ app/src/main/java/com/moc/sharecalc/ui/unitconverter/UnitConverterFragment.java
@@ -1,12 +1,17 @@
 package com.moc.sharecalc.ui.unitconverter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +19,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.moc.sharecalc.R;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,7 +33,10 @@ public class UnitConverterFragment extends Fragment {
                 ViewModelProviders.of(this).get(UnitConverterViewModel.class);
         View root = inflater.inflate(R.layout.fragment_unit_converter, container, false);
 
-        setUnitOptions(root);
+        setUnitOptions(root);   // dynamically set contents of second spinner
+
+        Button convertButton = root.findViewById(R.id.convert_button);
+        EditText unitInput = root.findViewById(R.id.unit_input);
 
         return root;
     }
@@ -37,7 +46,6 @@ public class UnitConverterFragment extends Fragment {
         Spinner convertOptionsSpinner = root.findViewById(R.id.convert_options);
 
         // dynamically set contents of second spinner
-
         unitTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()  {
             @Override
             public void onItemSelected(AdapterView<?> arg, View view, int position, long id)  {
@@ -53,6 +61,10 @@ public class UnitConverterFragment extends Fragment {
         });
     }
 
+    /*
+        * @return the correct ArrayAdapter to set the units Spinner to the correct conversion units
+        * option
+     */
     public ArrayAdapter getUnitOptions(int position)  {
         switch (position)  {
             case 1:
@@ -80,5 +92,38 @@ public class UnitConverterFragment extends Fragment {
                 getActivity().getBaseContext(),
                 R.array.default_units,
                 android.R.layout.simple_spinner_item);
+    }
+
+    public ArrayList getConversions(int position, double inputToConvert)  {
+        switch (position)  {
+            case 1:
+                return volumeConversions(inputToConvert);
+            case 2:
+                return lengthConversions(inputToConvert);
+            case 3:
+                return weightMassConversions(inputToConvert);
+            case 4:
+                return temperatureConversions(inputToConvert);
+
+        }
+        // return default units if user selection is not a valid conversion type
+        String[] resourceArray = getResources().getStringArray(R.array.default_units);
+        return new ArrayList(Arrays.asList(resourceArray));
+    }
+
+    public ArrayList volumeConversions(double convert)  {
+        return new ArrayList<String>();
+    }
+
+    public ArrayList lengthConversions(double convert)  {
+        return new ArrayList<String>();
+    }
+
+    public ArrayList weightMassConversions(double convert)  {
+        return new ArrayList<String>();
+    }
+
+    public ArrayList temperatureConversions(double convert)  {
+        return new ArrayList<String>();
     }
 }
