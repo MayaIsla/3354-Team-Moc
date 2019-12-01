e20cea910b63311a7d8e59841d47e09d7dd383ac
Alexis Vu <vualexis@outlook.com>
2019-12-01 06:03:02 -0600

    add @Before for unit converter UI unit tests

--- app/src/test/java/com/moc/sharecalc/unitutil/UnitConverterUITests.java
+++ app/src/test/java/com/moc/sharecalc/unitutil/UnitConverterUITests.java
@@ -1,4 +1,31 @@
 package com.moc.sharecalc.unitutil;
 
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.moc.sharecalc.R;
+import android.widget.ArrayAdapter;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.moc.sharecalc.ui.unitconverter.*; 
+
 public class UnitConverterUITests {
+
+	private Spinner unitTypeSpinner; 
+	private Spinner unitSpinner; 
+	private ArrayAdapter<String> unitTypeAdapter; 
+	private ArrayAdapter<String> unitAdapter; 
+	private View currentView; 
+
+	@Before 
+	public void initialize()  {
+		currentView = inflater.inflate(R.layout.fragment_unit_converter, container, false);
+		unitTypeSpinner = currentView.findViewById(R.id.unit_type_spinner); 
+		unitSpinner = currentView.findViewById(R.id.unit_spinner); 
+		unitTypeAdapter = unitTypeSpinner.getAdapter(); 
+		unitAdapter = unitSpinner.getAdapter(); 
+	}
+
+
 }
