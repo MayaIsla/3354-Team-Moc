6fb23b888964fe83ce74f1b35e42ea2be5bdc10a
amz170130 <amz170130@utdallas.edu>
2019-12-04 22:50:50 -0600

    Added documentation/comments to UnitConversionTests

--- app/src/test/java/com/moc/sharecalc/unitutil/UnitConversionTests.java
+++ app/src/test/java/com/moc/sharecalc/unitutil/UnitConversionTests.java
@@ -20,6 +20,14 @@ class UnitConversionTests {
     void basicConvertTest(double amount, Unit unit, double toAmount, Unit toUnit) {
         assertEquals(toAmount, unit.convertTo(toUnit, amount), 0.5);
     }
+
+    /**
+     * Unit test for all unit conversions. Asserts that the first arguments of Arguements.of() is equal to the
+     * third argument, using second and fourth arguments to perform the conversion; the second argument being
+     * the base unit and the fourth the unit to convert to.
+     *
+     * @return Stream of arguments that test the conversion between units
+     */
     private static Stream<Arguments> basicConvertTestProvider() {
         return Stream.of(
                 // Because temperature conversions are special, it helps to test them extra thoroughly
@@ -35,6 +43,7 @@ class UnitConversionTests {
                 Arguments.of(55, Unit.KELVIN, -360.7, Unit.FAHRENHEIT),
                 Arguments.of(55, Unit.KELVIN, 55, Unit.KELVIN),
 
+                //Tests for conversions between various lengths
                 Arguments.of(55, Unit.METERS, 55, Unit.METERS),
                 Arguments.of(55, Unit.METERS, 55E2, Unit.CENTIMETERS),
                 Arguments.of(55, Unit.FEET, 660, Unit.INCHES),
@@ -42,6 +51,7 @@ class UnitConversionTests {
                 Arguments.of(55E-5, Unit.MILES, 8.851E2, Unit.MILLIMETERS),
                 Arguments.of(55E-5, Unit.DECAMETERS, 5500, Unit.MICROMETERS),
 
+                //Tests for conversions between various volumes
                 Arguments.of(10, Unit.LITERS, 10, Unit.LITERS),
                 Arguments.of(10, Unit.LITERS, 10E3, Unit.MILLILITERS),
                 Arguments.of(10, Unit.LITERS, 2028.84, Unit.TEASPOONS),
@@ -52,6 +62,7 @@ class UnitConversionTests {
                 Arguments.of(10, Unit.LITERS, 10.5669, Unit.QUARTS),
                 Arguments.of(10, Unit.LITERS, 2.64172, Unit.GALLONS),
 
+                //Tests for conversions between various masses/weights.
                 Arguments.of(45, Unit.KILOGRAMS, 45, Unit.KILOGRAMS),
                 Arguments.of(45, Unit.KILOGRAMS, 45E3, Unit.GRAMS),
                 Arguments.of(45, Unit.KILOGRAMS, 45E6, Unit.MILLIGRAMS),
