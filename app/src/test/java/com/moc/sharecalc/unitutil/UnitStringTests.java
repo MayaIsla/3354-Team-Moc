6fdeb0b127802f1fddf13b1d1bc9c0f1df891369
Alexis Vu <vualexis@outlook.com>
2019-12-04 23:15:43 -0600

    add UnitStringTests test class
    assures that toString() method from Unit enum properly populates second spinner on unit converter UI in friendly, human-readable fashion

---
+++ app/src/test/java/com/moc/sharecalc/unitutil/UnitStringTests.java
@@ -0,0 +1,48 @@
+package com.moc.sharecalc.unitutil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+/*
+    This unit testing class determines whether the second spinner in the unit converter UI is being
+    properly populated with unit bases from the Unit enum.
+ */
+public class UnitStringTests {
+
+    private String kilogramsString;
+    private String gramsString;
+    private String milligramsString;
+    private String centigramsString;
+    private String ouncesString;
+    private String poundsString;
+    private String tonsString;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    /*
+        compares the friendly, human-readable string to the value that should be in the second
+        spinner
+        e.g. Unit.KILOGRAMS --> 'kilograms'
+     */
+    @Test
+    public void correctMassWeightSpinnerTest()  {
+        assertEquals(Unit.KILOGRAMS.toString(), "kilograms");
+        assertEquals(Unit.GRAMS.toString(), "grams");
+        assertEquals(Unit.MILLIGRAMS.toString(), "milligrams");
+        assertEquals(Unit.CENTIGRAMS.toString(), "centigrams");
+        assertEquals(Unit.OUNCES.toString(), "ounces");
+        assertEquals(Unit.POUNDS.toString(), "pounds");
+        assertEquals(Unit.TONS.toString(), "tons");
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
